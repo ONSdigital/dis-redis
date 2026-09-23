30eefb446e4fcf8b16bf489baac46644569fb84a
Ighomena Odebala <ighomena.odebala@ons.gov.uk>
2026-09-23 13:25:21 +0100

    Add go redis pipeliner mock

--- interfaces/client.go
+++ interfaces/client.go
@@ -3,6 +3,10 @@ package interfaces
 import "github.com/redis/go-redis/v9"
 
 //go:generate moq -out ../mocks/go-redis_client.go -pkg mocks . GoRedisClient
+//go:generate moq -out ../mocks/go-redis_pipeliner.go -pkg mocks . GoRedisPipeliner
 
 // RedisClient is an alias for redis.UniversalClient
 type GoRedisClient = redis.UniversalClient
+
+// GoRedisPipeliner is an alias for redis.Pipeliner.
+type GoRedisPipeliner = redis.Pipeliner
